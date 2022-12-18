f68a36b10af42fc5ee0e412e6e930da79b6157ed
Aki Nishimura <aki.nishimura@jhu.edu>
2022-12-18 10:31:12 -0500

    Unpack variables for better aesthetics

--- tests/testthat/test-against-lm.R
+++ tests/testthat/test-against-lm.R
@@ -1,7 +1,8 @@
 test_that("`hglm` coef/vcov estimate coincides with `lm` one", {
   data <- simulate_data(32, 4, intercept = 1, seed = 1918)
-  lm_out <- stats::lm(data$outcome ~ data$design + 0)
-  hglm_out <- hiper_glm(data$design, data$outcome)
+  design <- data$design; outcome <- data$outcome
+  lm_out <- stats::lm(outcome ~ design + 0)
+  hglm_out <- hiper_glm(design, outcome)
   expect_true(are_all_close(coef(hglm_out), coef(lm_out)))
   expect_true(are_all_close(  
     as.vector(vcov(hglm_out)), as.vector(vcov(lm_out))
--- tests/testthat/test-grad.R
+++ tests/testthat/test-grad.R
@@ -1,8 +1,9 @@
 test_that("linear model's analytical gradient is close to numerical one", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  design <- data$design; outcome <- data$outcome
   loglik_func <- function (coef) { 
-    calc_linear_loglik(coef, data$design, data$outcome)
+    calc_linear_loglik(coef, design, outcome)
   }
   set.seed(615)
   n_test <- 10
@@ -10,7 +11,7 @@ test_that("linear model's analytical gradient is close to numerical one", {
   for (i in 1:n_test) {
     if (!grads_are_close) break
     regcoef <- rnorm(n_pred)
-    analytical_grad <- calc_linear_grad(regcoef, data$design, data$outcome)
+    analytical_grad <- calc_linear_grad(regcoef, design, outcome)
     numerical_grad <- approx_grad_via_finite_diff(loglik_func, regcoef)
     grads_are_close <- are_all_close(
       analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
@@ -22,8 +23,9 @@ test_that("linear model's analytical gradient is close to numerical one", {
 test_that("logit model's analytical gradient is close to numerical one", {
   n_obs <- 32; n_pred <- 4
   data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
   loglik_func <- function (coef) { 
-    calc_logit_loglik(coef, data$design, data$outcome)
+    calc_logit_loglik(coef, design, outcome)
   }
   set.seed(615)
   n_test <- 10
@@ -31,7 +33,7 @@ test_that("logit model's analytical gradient is close to numerical one", {
   for (i in 1:n_test) {
     if (!grads_are_close) break
     regcoef <- rnorm(n_pred)
-    analytical_grad <- calc_logit_grad(regcoef, data$design, data$outcome)
+    analytical_grad <- calc_logit_grad(regcoef, design, outcome)
     numerical_grad <- approx_grad_via_finite_diff(loglik_func, regcoef)
     grads_are_close <- are_all_close(
       analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
