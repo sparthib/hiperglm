7147dc7f3b507557b89e804df0b9e307c5742167
Aki Nishimura <aki.nishimura@jhu.edu>
2022-12-18 10:20:24 -0500

    Add func to calc loglik and grad for logit model

--- R/loglik_and_deriv.R
+++ R/loglik_and_deriv.R
@@ -10,3 +10,29 @@ calc_linear_grad <- function(reg_coef, design, outcome, noise_var = 1) {
   grad <- as.vector(grad)
   return(grad)
 }
+
+calc_logit_loglik <- function(
+    reg_coef, design, n_success, n_trial = NULL
+) {
+  if (is.null(n_trial)) {
+    # Assume binary outcome unless otherwise specified.
+    n_trial <- rep(1, length(n_success))
+  }
+  logit_prob <- design %*% reg_coef
+  loglik <- sum(n_success * logit_prob - n_trial * log(1 + exp(logit_prob)))
+    # TODO: improve numerical stability for logit_prob >> 1
+  return(loglik)
+}
+
+calc_logit_grad <- function(
+    reg_coef, design, n_success, n_trial = NULL
+) {
+  if (is.null(n_trial)) {
+    n_trial <- rep(1, length(n_success))
+  }
+  logit_prob <- design %*% reg_coef
+  predicted_prob <- 1 / (1 + exp(-logit_prob))
+  grad <- t(design) %*% (n_success - n_trial * predicted_prob)
+  grad <- as.vector(grad)
+  return(grad)
+}
