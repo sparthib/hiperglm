acbd00d10175dd13e2ca2a21c49f9505b5c97a86
Aki Nishimura <aki.nishimura@jhu.edu>
2022-12-20 11:14:02 -0500

    Test logit fit via optim against `glm` fit

--- tests/testthat/test-against-stats-pkg.R
+++ tests/testthat/test-against-stats-pkg.R
@@ -1,4 +1,4 @@
-test_that("`hglm` coef/vcov estimate coincides with `lm` one", {
+test_that("`hglm` linear model est coincides with `lm` one", {
   data <- simulate_data(32, 4, intercept = 1, seed = 1918)
   design <- data$design; outcome <- data$outcome
   lm_out <- stats::lm(outcome ~ design + 0)
@@ -7,4 +7,17 @@ test_that("`hglm` coef/vcov estimate coincides with `lm` one", {
   expect_true(are_all_close(  
     as.vector(vcov(hglm_out)), as.vector(vcov(lm_out))
   ))
+})
+
+test_that("`hglm` logit model est coincides with `glm` one", {
+  data <- simulate_data(32, 4, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  glm_out <- stats::glm(outcome ~ design + 0, family = binomial('logit'))
+  hglm_out <- hiper_glm(
+    design, outcome, model = 'logit', option = list(mle_solver = 'BFGS')
+  )
+  expect_true(are_all_close(
+    coef(hglm_out), coef(glm_out), abs_tol = Inf, rel_tol = 1e-3
+  ))
+  # TODO: test covariance estimate
 })
