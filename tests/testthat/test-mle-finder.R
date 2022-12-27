4c94b631f80fa299ba9f39229d906e63ce3f8495
Aki Nishimura <aki.nishimura@jhu.edu>
2022-12-27 18:44:34 -0500

    Unit test newton update

--- tests/testthat/test-mle-finder.R
+++ tests/testthat/test-mle-finder.R
@@ -23,3 +23,16 @@ test_that("newton and bfgs outputs coincide on logit model", {
     coef(via_newton_out), coef(via_bfgs_out), abs_tol = 1e-2, rel_tol = 1e-2
   ))
 })
+
+test_that("vanilla/weighted least-sq Newton updates coincide", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  design <- data$design; outcome <- data$outcome
+  set.seed(615)
+  init_coef <- rnorm(n_pred)
+  wls_updated_coef <- 
+    take_one_newton_step(init_coef, design, outcome, solver = "weighted-leqst-sq")
+  ne_updated_coef <- 
+    take_one_newton_step(init_coef, design, outcome, solver = "normal-eq")
+  expect_true(are_all_close(wls_updated_coef, ne_updated_coef))
+})
