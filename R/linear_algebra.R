9fa0562b3ec22cd42c38f26928ee14c13423524d
Aki Nishimura <aki.nishimura@jhu.edu>
2023-01-01 12:20:22 -0500

    Avoid expensive calc of Q matrix

--- R/linear_algebra.R
+++ R/linear_algebra.R
@@ -4,11 +4,11 @@ solve_least_sq_via_qr <- function(X, y, weight = NULL) {
     X <- outer(sqrt(weight), rep(1, ncol(X))) * X
     y <- sqrt(weight) * y
   }
-  qr_decomp <- qr_wrapper(X) 
-  Q <- qr_decomp$Q; R <- qr_decomp$R
-  solution <- backsolve(R, t(Q) %*% y)
+  out <- qr_wrapper(X)
+  qr_decomp <- out$qr_decomp; R <- out$R
+  solution <- solve(qr_decomp, y)
   solution <- as.vector(solution)
-  return(list(solution = solution, Q = Q, R = R))
+  return(list(solution = solution, R = R))
 }
 
 # Find an inverse of the gram matrix t(X) %*% X from the QR factor of X
@@ -16,11 +16,14 @@ invert_gram_mat_from_qr <- function(R) {
   return(chol2inv(R))
 }
 
-# Returns un-pivoted (Q, R) factors so that X == Q %*% R
-qr_wrapper <- function(X) {
-  qr_result <- qr(X)
-  Q <- qr.Q(qr_result)
-  R <- qr.R(qr_result)
-  R <- R[ , order(qr_result$pivot)]
-  return(list(Q = Q, R = R))
+# Return un-pivoted (Q, R) factors so that X == Q %*% R
+qr_wrapper <- function(X, require_Q = FALSE) {
+  qr_decomp <- qr(X)
+  R <- qr.R(qr_decomp)
+  R <- R[ , order(qr_decomp$pivot)]
+  result <- list(qr_decomp = qr_decomp, R = R)
+  if (require_Q) {
+    result$Q <- qr.Q(qr_decomp)
+  }
+  return(result)
 }
