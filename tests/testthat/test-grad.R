8d41fa06f864d90b69020a356f6da2fd5e9f405a
Aki Nishimura <aki.nishimura@jhu.edu>
2023-02-03 13:52:27 -0500

    Test grad calc for logit model

--- tests/testthat/test-grad.R
+++ tests/testthat/test-grad.R
@@ -18,3 +18,24 @@ test_that("linear model's analytical gradient is close to numerical one", {
   }
   expect_true(grads_are_close)
 })
+
+test_that("logit model's analytical gradient is close to numerical one", {
+  n_obs <- 32; n_pred <- 4
+  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
+  loglik_func <- function (coef) { 
+    calc_logit_loglik(coef, data$design, data$outcome)
+  }
+  set.seed(615)
+  n_test <- 10
+  grads_are_close <- TRUE
+  for (i in 1:n_test) {
+    if (!grads_are_close) break
+    regcoef <- rnorm(n_pred)
+    analytical_grad <- calc_logit_grad(regcoef, data$design, data$outcome)
+    numerical_grad <- approx_grad_via_finite_diff(loglik_func, regcoef)
+    grads_are_close <- are_all_close(
+      analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
+    )
+  }
+  expect_true(grads_are_close)
+})
