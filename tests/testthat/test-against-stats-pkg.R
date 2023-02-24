5ae55e9329e80de13451413690d88c9d7ed346d4
Aki Nishimura <aki.nishimura@jhu.edu>
2023-02-23 22:37:42 -0500

    Test hglm on binomial, non-binary outcome

--- tests/testthat/test-against-stats-pkg.R
+++ tests/testthat/test-against-stats-pkg.R
@@ -9,7 +9,7 @@ test_that("`hglm` linear model est coincides with `lm` one", {
   ))
 })
 
-test_that("`hglm` logit model est coincides with `glm` one", {
+test_that("`hglm` logit model est coincides with `glm` one for binary outcome", {
   data <- simulate_data(32, 4, model = 'logit', seed = 1918)
   design <- data$design; outcome <- data$outcome
   glm_out <- stats::glm(outcome ~ design + 0, family = binomial('logit'))
@@ -17,5 +17,27 @@ test_that("`hglm` logit model est coincides with `glm` one", {
   expect_true(are_all_close(coef(hglm_out), coef(glm_out)))
   expect_true(are_all_close(
     as.vector(vcov(hglm_out)), as.vector(vcov(glm_out))
+  ))
+})
+
+test_that("`hglm` logit model est coincides with `glm` one for binomial outcome", {
+  set.seed(615)
+  n_obs <- 32; n_pred <- 4
+  n_trial <- 1L + rpois(n_obs, lambda = 1)
+  data <- simulate_data(
+    n_obs, n_pred, model = 'logit', seed = 1918, 
+    option = list(n_trial = n_trial)
+  )
+  design <- data$design; outcome <- data$outcome
+  n_success <- outcome$n_success; n_trial <- outcome$n_trial
+  glm_out <- stats::glm(
+    cbind(n_success, n_trial - n_success) ~ design + 0, 
+    family = binomial('logit')
+  )
+  hglm_out <- hiper_glm(design, outcome, model = 'logit')
+  expect_true(are_all_close(coef(hglm_out), coef(glm_out)))
+  expect_true(are_all_close(
+    as.vector(vcov(hglm_out)), as.vector(vcov(glm_out)), 
+    abs_tol = Inf, rel_tol = 1e-3
   ))
 })
