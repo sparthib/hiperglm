ddee2096e124646796bd74b04383ad144614de63
Aki Nishimura <aki.nishimura@jhu.edu>
2024-02-02 22:54:26 -0500

    Introduce helper func for testing grad to unify tests across different models

--- tests/testthat/test-grad.R
+++ tests/testthat/test-grad.R
@@ -1,43 +1,51 @@
-test_that("linear model's analytical gradient is close to numerical one", {
+compare_anlytical_and_numerical_grad <- function(
+  model, n_obs = 32, n_pred = 4, n_test = 10, data_seed = 1918, loc_seed = 615
+) {
   n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = 'linear', seed = 1918)
+  data <- simulate_data(n_obs, n_pred, model, seed = data_seed)
   design <- data$design; outcome <- data$outcome
+  # The `do.call` trick below might seem like a clever solution and a 
+  # simpler-to-implement alternative to S3 methods. However, it tends to make 
+  # the code less readable and harder to maintain. For example, you wouldn't 
+  # know the exact function being called by `sprintf("calc_%s_loglik", model)` 
+  # without reading other parts of code. It also obscures the usage of those 
+  # functions within codebase; e.g. `git grep "calc_logit_grad"` would fail to 
+  # detect its usage here. The trick is used here only for didactic/illustrative 
+  # purpose --- I advise against its use in general.
   loglik_func <- function (coef) { 
-    calc_linear_loglik(coef, design, outcome)
+    do.call(
+      sprintf("calc_%s_loglik", model), 
+      list(coef, design, outcome)
+    )
   }
-  set.seed(615)
-  n_test <- 10
+  grad_func <- function (coef) {
+    do.call(
+      sprintf("calc_%s_grad", model), 
+      list(coef, design, outcome)
+    )
+  }
+  set.seed(loc_seed)
   grads_are_close <- TRUE
   for (i in 1:n_test) {
     if (!grads_are_close) break
     regcoef <- rnorm(n_pred)
-    analytical_grad <- calc_linear_grad(regcoef, design, outcome)
+    analytical_grad <- grad_func(regcoef)
     numerical_grad <- approx_grad_via_finite_diff(loglik_func, regcoef)
     grads_are_close <- are_all_close(
       analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
     )
   }
-  expect_true(grads_are_close)
+  return(grads_are_close)
+}
+
+test_that("linear model's analytical gradient is close to numerical one", {
+  expect_true(
+    compare_anlytical_and_numerical_grad("linear")
+  )
 })
 
 test_that("logit model's analytical gradient is close to numerical one", {
-  n_obs <- 32; n_pred <- 4
-  data <- simulate_data(n_obs, n_pred, model = 'logit', seed = 1918)
-  design <- data$design; outcome <- data$outcome
-  loglik_func <- function (coef) { 
-    calc_logit_loglik(coef, design, outcome)
-  }
-  set.seed(615)
-  n_test <- 10
-  grads_are_close <- TRUE
-  for (i in 1:n_test) {
-    if (!grads_are_close) break
-    regcoef <- rnorm(n_pred)
-    analytical_grad <- calc_logit_grad(regcoef, design, outcome)
-    numerical_grad <- approx_grad_via_finite_diff(loglik_func, regcoef)
-    grads_are_close <- are_all_close(
-      analytical_grad, numerical_grad, abs_tol = Inf, rel_tol = 1e-3
-    )
-  }
-  expect_true(grads_are_close)
+  expect_true(
+    compare_anlytical_and_numerical_grad("logit")
+  )
 })
